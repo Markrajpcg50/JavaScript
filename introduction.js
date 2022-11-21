c8c45d86cb50a01de64b2dbcf726be21612d6f93
Markrajpcg50 <112145938+Markrajpcg50@users.noreply.github.com>
2022-11-21 12:29:17 +0530

    object creation

--- introduction.js
+++ introduction.js
@@ -20,3 +20,38 @@ if(a!==b)
 {
     console.log("=== operator does not do type casting");
 }
+
+if(a){
+    console.log("a is a value and it will be true");
+}
+else{
+    console.log("a is not a value/null and it will be false");
+}
+if(b){
+    console.log("a is a string and it will be true");
+}
+else{
+    console.log("a is empty string/null and it will be false");
+}
+
+var myString="Hello";
+// creating objects
+var myObj={};
+console.log(myObj);
+// adding state to an object
+myObj.prop="Name";
+console.log(myObj);
+myObj.prop2="35";
+console.log(myObj);
+console.log(myObj.prop);
+console.log(myObj.prop2);
+
+// creating objects using object literals
+var myObj2={
+    "name":"Client",
+    "Org":"ABC Inc",
+    "Estd": 2022
+}
+console.log(myObj2);
+console.log("Accessing the state using the dot notation "+myObj2.name);
+console.log("Accessing the state using the square bracket notation "+myObj2["name"]);
