e44029237dfb34676956c2154c2eaeda47ff5e82
Markrajpcg50 <112145938+Markrajpcg50@users.noreply.github.com>
2022-11-21 11:05:21 +0530

    operators and control statements

--- introduction.js
+++ introduction.js
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+var a=10;
+var b="10";
+var c=false;
+var d=10;
+console.log(a);
+console.log(b);
+console.log(c);
+
+if(a==d)
+{
+    console.log(a,"is equal to",d);
+}
+if(a==b)
+{
+    console.log("== operator does the type casting");
+}
+if(a!==b)
+{
+    console.log("=== operator does not do type casting");
+}
